9259850921ab99b48585a7f8e10089fe53ac6159
Adam Fernandes <adaferna@student.42lisboa.com>
2026-08-26 20:16:07 +0100

    A-Maze-ing-draft - wip

--- maze_generator/_maze_gen.py
+++ maze_generator/_maze_gen.py
@@ -4,6 +4,9 @@ from enum import IntFlag
 from typing import Generator
 import random
 
+# TODO: Remove global after config mgmt
+glob_mode = "perfect"
+
 
 class Matrix:
     def __init__(self, row_max: int, col_max: int) -> None:
@@ -14,33 +17,78 @@ class Matrix:
 
     def pregen_maze(self) -> None:
         """"define constraint before generating maze"""
+        # TODO: define logo
+        logoset = self.gen_logo_coordset()
         for row in range(self.row_max):
             for col in range(self.col_max):
                 border = False
+                logo = False
+                # border
                 if (row == 0 or
                         col == 0 or
                         row == self.row_max - 1 or
                         col == self.col_max - 1):
                     border = True
-                self.matrix[row][col] = Case(row, col, border=border)
+                # logo
+                coord = (col, row)
+                print(coord)
+                if coord in logoset:
+                    logo = True
+                self.matrix[row][col] = Case(row, col,
+                                             border=border,
+                                             logo=logo)
 
-    def gen_logo(self) -> None:
+    # TODO: Maybe to moove in a subclass Logo
+    def gen_logo_coordset(self) -> set:
+        """ generate logo coordinates"""
+        if self.row_max <= 8 or self.col_max <= 8:
+            raise Exception('Grid too little for logo')
         r_center = self.row_max // 2
-        c_center = self.col_max
+        c_center = self.col_max // 2
+        logo_coords: list[tuple[int, int]] = []
         size = 3
-        # 4
-        #line 1
+        # 4 - from top to bottom
+        y = r_center - size
+        x = c_center - size
+        logo_coords.append((x, y))
+        for _ in range(size - 1):
+            y += 1
+            logo_coords.append((x, y))
+        for _ in range(size-1):
+            x += 1
+            logo_coords.append((x, y))
+        for _ in range(size-1):
+            y += 1
+            logo_coords.append((x, y))
+        # 2 - from top to bottom
+        y = r_center - size
+        x = c_center + 1
+        logo_coords.append((x, y))
+        for _ in range(size - 1):
+            x += 1
+            logo_coords.append((x, y))
+        for _ in range(size-1):
+            y += 1
+            logo_coords.append((x, y))
+        for _ in range(size-1):
+            x -= 1
+            logo_coords.append((x, y))
+        for _ in range(size-1):
+            y += 1
+            logo_coords.append((x, y))
+        for _ in range(size-1):
+            x += 1
+            logo_coords.append((x, y))
+        return set(logo_coords)
 
-
-        for row in range(self.row_max):
-            for col in range(self.col_max):
-                ...
     def debug_print_maze(self) -> None:
         for row in range(self.row_max):
             for col in range(self.col_max):
                 match self.matrix[row][col]:
                     case Case(border=True):
                         print("B", end="")
+                    case Case(logo=True):
+                        print("L", end="")
                     case Case(border=False):
                         print("0", end="")
                     case _:
@@ -69,16 +117,22 @@ class Matrix:
         print(bottom + "+")
 
 
-
 def gen_wall(matrix: Matrix) -> None:
     for row in range(matrix.row_max):
         for col in range(matrix.col_max):
-            wallset = gen_wallset(matrix, row, col)
-            matrix.matrix[row][col].wall = random.choice(tuple(wallset))
+            # logo
+            if matrix.matrix[row][col].logo:
+                matrix.matrix[row][col].wall = 15
+            else:
+                wallset = gen_wallset(matrix, row, col)
+                matrix.matrix[row][col].wall = random.choice(tuple(wallset))
 
 
 def gen_wallset(matrix: Matrix, row: int, col: int) -> set[int]:
-    baseset = Wall.BASESET
+    if glob_mode == "perfect":
+        baseset = Wall.PERFECTSET
+    else:
+        baseset = Wall.BASESET
     wallset: set[int] = set(Wall.BASESET.copy())
     to_close: set[int] = set()
     to_open: set[int] = set()
@@ -102,7 +156,11 @@ def gen_wallset(matrix: Matrix, row: int, col: int) -> set[int]:
             to_close.add(Dir.N)
         else:
             to_open.add(Dir.N)
-
+    # successor constraint (logo)
+    if col < matrix.col_max - 1 and matrix.matrix[row][col + 1].logo:
+        to_close.add(Dir.E)
+    if row < matrix.col_max - 1 and matrix.matrix[row + 1][col].logo:
+        to_close.add(Dir.S)
     for wall in to_close:
         # intersection
         wallset = wallset & set(n for n in baseset if n & wall)
@@ -114,6 +172,7 @@ def gen_wallset(matrix: Matrix, row: int, col: int) -> set[int]:
 
 class Wall:
     BASESET = frozenset((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15))
+    PERFECTSET = frozenset((1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14))
 
 
 class Dir(IntFlag):
@@ -130,16 +189,20 @@ class Case:
                  x: int,
                  y: int,
                  border: bool = False,
-                 logo: bool = False) -> None:
+                 logo: bool = False,
+                 logo_coridor: bool = False,
+                 logo_top: bool = False) -> None:
         self.x = x
         self.y = y
         self.border = border  # cannot be open the same way
         self.logo = logo
+        self.logo_coridor = False
+        self.logo_top = False
         self.wall: int = 0
 
 
 if __name__ == "__main__":
-    test_grid = Matrix(7, 7)
+    test_grid = Matrix(10, 10)
     test_grid.pregen_maze()
     test_grid.debug_print_maze()
     gen_wall(test_grid)
